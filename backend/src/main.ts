88c05dfefa4a1dbafe58ea4caa1b2a58478652f9
Aleksandr Petrov <petrov.aleksr@gmail.com>
2024-06-16 15:03:18 +0500

    fix: небольшие поправки

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -10,12 +10,11 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
   const serverPort = configService.get<number>('SERVER_PORT');
-  const frontendUrl =
-    configService.get<string>('FRONTEND_URL');
+  const frontendUrl = configService.get<string>('FRONTEND_URL');
 
   app.enableCors({
     origin: frontendUrl,
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
   });
   app.useGlobalPipes(new ValidationPipe({ transform: true }));
